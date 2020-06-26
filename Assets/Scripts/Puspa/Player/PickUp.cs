86f37e982e8e28fed2e2a31032fec93d41980b2e
puspanegara <54850975+puspanegara@users.noreply.github.com>
2020-06-26 07:59:39 +0700

    - Belum sempat unutk memasukan item yang ke prefab - Unity error

---
+++ Assets/Scripts/Puspa/Player/PickUp.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public GameObject itemButton;
+    private Inventory inventory;
+
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            for (int i = 0; i < inventory.slots.Length; i++)
+            {
+                if (inventory.isFull[i] == false)
+                {
+                    if (Input.GetKeyDown(KeyCode.E))
+                    {
+                        Debug.Log("Berhasil");
+                        inventory.isFull[i] = true;
+                        Instantiate(itemButton, inventory.slots[i].transform, false);
+                        Destroy(gameObject);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}

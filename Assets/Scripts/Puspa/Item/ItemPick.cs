38668469b154c319c6898a4a53f7e9c023e308cd
puspanegara <54850975+puspanegara@users.noreply.github.com>
2020-06-26 12:25:59 +0700

    no message

---
+++ Assets/Scripts/Puspa/Item/ItemPick.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPick : MonoBehaviour
+{
+    public GameObject itemButton;
+    private Inventory inventory;
+    public string buttonTake;
+    
+
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            for (int i = 0; i < inventory.slots.Length; i++)
+            {
+                if (inventory.isFull[i] == false)
+                {
+                    if (Input.GetButtonDown(buttonTake))
+                    {
+                        Debug.Log("Berhasil");
+                        inventory.isFull[i] = true;
+                        Instantiate(itemButton, inventory.slots[i].transform, false);
+                        Destroy(gameObject);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/UI_Inventory.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Inventory : MonoBehaviour
+{
+    public GameObject inventoryUI;
+    public string buttonSelect;
+    void Update()
+    {
+        OpenCloseInven();
+    }
+    public void OpenCloseInven()
+    {
+        if(Input.GetButtonDown("buttonSelect"))
+        {
+
+        }
+    }
+}

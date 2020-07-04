fe5a2022e43bfe4491f643ff62931490b1acfd55
puspanegara <54850975+puspanegara@users.noreply.github.com>
2020-07-04 10:04:08 +0700

    no message

---
+++ Assets/Scripts/Puspa/PopUp/BukaTutup.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BukaTutup : MonoBehaviour
+{
+    public GameObject panel;
+    public bool isClose;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isClose = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (isClose == true)
+                {
+                    panel.SetActive(true);
+                    isClose = false;
+                }
+                else
+                {
+                    panel.SetActive(false);
+                    isClose = true;
+                }
+            }
+        }
+    }
+}
